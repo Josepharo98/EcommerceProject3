5bf98bb686505a3087038e890c9a5d104c9fe071
Joseph Aro <josepharo88@gmail.com>
2024-01-04 19:19:17 -0500

    outline and first push

---
+++ client/src/Types/Products.jsx
@@ -0,0 +1,12 @@
+export type Product = {
+    name: String
+    slug: String
+    image: String
+    category: String
+    brand: String
+    price: Number
+    CountInStock: Number
+    Description: String
+    Rating: Number
+    NumReviews: Number
+}
